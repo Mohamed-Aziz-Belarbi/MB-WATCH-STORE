afbf62de3c30b0e7e02d44e17fac23006a9d20da
Mohamed-Aziz-Belarbi <aziz.belarbi1998@gmail.com>
2023-03-07 15:04:30 +0100

    doneLogin

---
+++ main1.js
@@ -0,0 +1,43 @@
+var users=[{
+
+username:"yassine",
+email:"yassinemiraoui4@gmail.com",
+password:"azerty1234"
+},{
+    username:"aziz",
+    email:"aziz.belarbi1998@gmail.com",
+    password:"aziz1234"
+
+},{
+username:"arbi",
+email:"arbibenmoussi@gmail.com",
+password:"arbi2020"
+}]
+// var email1 = $("#emai").val()
+// var pw1 = $("#pw").val()
+// console.log("hi")
+//var x =''
+//var y = ''
+
+//function checkUser(event) {
+   // event.preventDefault();
+   // var email1 = $("#emai").val();
+   // var pw1 = $("#pw").val();
+   // x = email1
+    //y = pw1
+   // console.log("Email:", email1, "Password:", pw1);
+ // }
+
+  //console.log(x,y)
+
+
+// function checkUser(){
+//     for( var i=0;users.length;i++){
+//         if ((($("#email").val()===users[i].email)) && (($("#passWord").val()===users[i].password))){
+//      $("#buttonIn").
+//         }
+//     }
+// }
+
+
+// i create this function called checkUser but it works but we didn't find a solution to link it with page 
