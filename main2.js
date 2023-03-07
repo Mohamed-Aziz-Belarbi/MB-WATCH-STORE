d4141530303c1370b910886bc149c48da17eb2b7
yassine-miraoui <yassinemiraoui4@gmail.com>
2023-03-07 17:12:33 +0100

    done

--- main2.js
+++ main2.js
@@ -1,9 +1,23 @@
-// var gallery = document.getElementById("gallery")
+var gallery = document.getElementById("gallery");
 
-// window.onmousemove= e =>{
-//     var mouseX= e.clientX;
-//     mouseY= e.clientY;
+window.onmousemove = function(e) {
+  var mouseX = e.clientX,
+      mouseY = e.clientY;
 
-// }
-// var xDectimal = mouseX / window.innerWidth,
-// // yDectimal = 
+  var xDectimal = mouseX / window.innerWidth,
+      yDectimal = mouseY / window.innerHeight;
+
+  var maxX = gallery.offsetWidth - window.innerWidth,
+      maxY = gallery.offsetHeight - window.innerHeight;
+
+  var panX = maxX * xDectimal * -1,
+      panY = maxY * yDectimal * -1;
+
+  gallery.animate({
+    transform: 'translate(' + panX + 'px,' + panY + 'px)'
+  }, {
+    duration: 4000,
+    fill: "forwards",
+    easing: "ease"
+  });
+};
